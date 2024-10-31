232cf7188c90236be4c46141fffdd9162b3794b6
mutua-muvevi <samstore637@gmail.com>
2024-10-31 18:45:43 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//root/index page
+
+//package imports
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+
+//custom module imports and initialization
+const app = express();
